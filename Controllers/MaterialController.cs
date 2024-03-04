5f28120110f54275c1abd8eab491fe26783422d2
Maroosek <126722704+Maroosek@users.noreply.github.com>
2024-03-04 20:52:31 +0100

    Added Material Controller Added Index for materials

---
+++ Controllers/MaterialController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OSRSHelper.Data;
+
+namespace OSRSHelper.Controllers
+{
+	public class MaterialController : Controller
+	{
+		private readonly OSRSDbContext DbContext;
+
+		public MaterialController(OSRSDbContext dbContext)
+		{
+			DbContext = dbContext;
+		}
+		public async Task<IActionResult> Index()
+		{
+			var materials = await DbContext.Materials.ToListAsync();
+			return View(materials);
+		}
+	}
+}
